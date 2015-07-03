ab0b76e375a87701ada7e344ae0294c029dbd07e
Victor Paul <victorpaul.war@gmail.com>
2015-07-03 15:20:34 +0300

    lille refactoring

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function orderCarsOnTheStart(){
 window.onload = function(){
     var Q = window.Q = Quintus({ audioSupported: [ 'mp3' ] }).include("Sprites, Scenes,Anim,UI,Touch,Audio").setup({ width: 1920, height: 1080 }).touch().enableSound();
 
-    Q.load(["arrow.png","wind.png","song.mp3","3.png","1_animation.png","2_animation.png","4_animation.png"], function() {
+    Q.load(["PENEK.png","tree_up.png","tree_right.png","Tree_down_2.png","Tree_down_1.png","SENO_right.png","SENO_keft.png","ZNAK.png","down_left.png","down_right.png","center.png","arrow.png","wind.png","song.mp3","3.png","1_animation.png","2_animation.png","4_animation.png"], function() {
 
         Q.sheet("ment","2_animation.png",{tilew: 90,tileh: 45,sx: 0,sy: 0});
         Q.sheet("car","1_animation.png",{tilew: 75,tileh: 33,sx: 0,sy: 0});
@@ -66,22 +66,6 @@ window.onload = function(){
           }
         });
 
-        Q.Sprite.extend("Wind", {
-            init: function(p) {
-                p = p || {};
-                p.asset = "wind.png";
-                p.points = [[0,0]];
-                p.cx = 166/2;
-                p.cy = 166/2;
-                p.type = Q.SPRITE_UI;
-                p.scale = 1;
-                p.omega = 50;
-                this._super(p);
-            },
-            step: function(dt) {
-                 this.p.angle += dt * (this.p.omega);
-            }
-        });
 
         fbRef.child("cars").on("child_removed", function(snapshot) {
           if(cars[snapshot.key()]){
@@ -102,6 +86,35 @@ window.onload = function(){
                 p.cy = 300;
                 p.scale = 0.03;
                 this._super(p);
+            }
+        });
+
+        Q.Sprite.extend("Decor", {
+            init: function(p) {
+                p = p || {};
+                p.points = [[0,0]];
+                p.cx = 0;
+                p.cy = 0;
+                p.type = Q.SPRITE_UI;
+                p.scale = 1;
+                this._super(p);
+            }
+        });
+
+        Q.Sprite.extend("Wind", {
+            init: function(p) {
+                p = p || {};
+                p.asset = "wind.png";
+                p.points = [[0,0]];
+                p.cx = 166/2;
+                p.cy = 166/2;
+                p.type = Q.SPRITE_UI;
+                p.scale = 1;
+                p.omega = 50;
+                this._super(p);
+            },
+            step: function(dt) {
+                 this.p.angle += dt * (this.p.omega);
             }
         });
 
@@ -242,11 +255,11 @@ window.onload = function(){
 
             //rectangles
             stageAccess.insert(new Q.Wall({w:520,h:120,x:0,y:0})); // top left block
-            stageAccess.insert(new Q.Wall({w:575,h:100,x:1920-575,y:1080-100})); //bottom right block
-            stageAccess.insert(new Q.Wall({w:250,h:700,x:-230,y:1265-450,angle:315})); // bottom left block
+            stageAccess.insert(new Q.Wall({w:575,h:100,x:1920-565,y:1080-80})); //bottom right block
+            stageAccess.insert(new Q.Wall({w:250,h:700,x:-260,y:1265-450,angle:316})); // bottom left block
 
             stageAccess.insert(new Q.Wall({w:500,h:290,x:430,y:520,angle:335})); // middle block1
-            stageAccess.insert(new Q.Wall({w:470,h:280,x:900,y:300})); // middle block2
+            stageAccess.insert(new Q.Wall({w:460,h:260,x:900,y:320})); // middle block2
 
             stageAccess.insert(new Q.Wall({w:50,h:180,x:200,y:200,angle:15}));// truck 1 tail
             stageAccess.insert(new Q.Wall({w:50,h:90,x:153,y:400,angle:345}));//truck 1 head
@@ -256,37 +269,40 @@ window.onload = function(){
 
             stageAccess.insert(new Q.Wall({w:65,h:150,x:1465,y:395,angle:15}));//who knows what is it
 
-            stageAccess.insert(new Q.Circle({x:700,y:80,r:30}));// pile 1 top
-            stageAccess.insert(new Q.Circle({x:1520,y:680,r:30}));// pile 2 middle in middle right
-            stageAccess.insert(new Q.Circle({x:1780,y:850,r:30}));// pile 3 almost bottom right
-            stageAccess.insert(new Q.Circle({x:135,y:580,r:25}));// pile 5 middle left
-            stageAccess.insert(new Q.Circle({x:70,y:650,r:30}));// pile 6 under middle left
+            //stageAccess.insert(new Q.Circle({x:700,y:80,r:30}));// pile 1 top
+            //stageAccess.insert(new Q.Circle({x:1520,y:680,r:30}));// pile 2 middle in middle right
+            stageAccess.insert(new Q.Circle({x:1747,y:820,r:25}));// pile 3 almost bottom right
+            stageAccess.insert(new Q.Circle({x:145,y:633,r:20}));// pile 5 middle left
+            //stageAccess.insert(new Q.Circle({x:70,y:650,r:30}));// pile 6 under middle left
+
+            stageAccess.insert(new Q.Circle({x:1530,y:715,r:5}));// pile 6 under middle left
 
             // 5 trees in row topp in middle
-            stageAccess.insert(new Q.Circle({x:1000,y:275,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1040,y:270,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1080,y:270,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1120,y:265,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1160,y:260,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1000,y:275,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1040,y:270,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1080,y:270,r:10}));// tree
+            stageAccess.insert(new Q.Circle({x:1102,y:232,r:5}));// tree
+            //stageAccess.insert(new Q.Circle({x:1160,y:260,r:10}));// tree
 
             // three tree in the middle
-            stageAccess.insert(new Q.Circle({x:990,y:660,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1040,y:630,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1070,y:610,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:990,y:660,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1040,y:630,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1070,y:610,r:10}));// tree
 
             // three tree in the right
-            stageAccess.insert(new Q.Circle({x:1850,y:500,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1880,y:510,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1880,y:570,r:10}));// tree
+            stageAccess.insert(new Q.Circle({x:1745,y:495,r:5}));// tree
+            //stageAccess.insert(new Q.Circle({x:1880,y:510,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1880,y:570,r:10}));// tree
 
             // 4 trees in row topp in middle
-            stageAccess.insert(new Q.Circle({x:960,y:1020,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1000,y:1000,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1040,y:960,r:10}));// tree
-            stageAccess.insert(new Q.Circle({x:1080,y:920,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:960,y:1020,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1000,y:1000,r:10}));// tree
+            //stageAccess.insert(new Q.Circle({x:1040,y:960,r:10}));// tree
+            stageAccess.insert(new Q.Circle({x:1068,y:920,r:5}));// tree
+            stageAccess.insert(new Q.Circle({x:1305,y:867,r:5}));// tree
 
-            stageAccess.insert(new Q.Wall({counter:true, w:500,h:10,x:1380,y:420}));// tree
-            stageAccess.insert(new Q.Wall({counter:true,w:10,h:500,x:700,y:820,angle:350,finish:true}));// tree
+            stageAccess.insert(new Q.Wall({counter:true, w:500,h:10,x:1380,y:420}));
+            stageAccess.insert(new Q.Wall({counter:true,w:10,h:500,x:700,y:820,angle:350,finish:true}));
 
             stage.insert(new Q.UI.Button({label: "Press to START RACE!",y: 70,x: 1750,fill: "#990000",border: 5,shadow: 10,shadowColor: "rgba(0,0,0,0.5)"}, function() {
               //Q.audio.play('song.mp3');
@@ -309,15 +325,29 @@ window.onload = function(){
             counter();
             }));
 
-          stageAccess.insert(new Q.Wind({x:184,y:850,z:2000}));
         }));
 
 
+        Q.scene("Foreground",new Q.Scene(function(stage) {
+          stage.insert(new Q.Decor({x:434,y:260,asset:"center.png"}));
+          stage.insert(new Q.Decor({x:0,y:695,asset:"down_left.png"}));
+          stage.insert(new Q.Wind({x:174,y:855,z:2}));
+          stage.insert(new Q.Decor({x:1350,y:953,asset:"down_right.png"}));
 
+          stage.insert(new Q.Decor({x:1715,y:750,asset:"SENO_right.png"}));
+          stage.insert(new Q.Decor({x:114,y:570,asset:"SENO_keft.png"}));
+          stage.insert(new Q.Decor({x:1510,y:610,asset:"ZNAK.png"}));
+          stage.insert(new Q.Decor({x:1700,y:380,asset:"tree_right.png"}));
+          stage.insert(new Q.Decor({x:1060,y:118,asset:"tree_up.png"}));
+          stage.insert(new Q.Decor({x:1350,y:953,asset:"PENEK.png"}));
+          stage.insert(new Q.Decor({x:1027,y:808,asset:"Tree_down_1.png"}));
+          stage.insert(new Q.Decor({x:1262,y:755,asset:"Tree_down_2.png"}));
+        }));
 
-        Q.stageScene("Race");
+        Q.stageScene("Race",0);
+        Q.stageScene("Foreground",1);
 
-        /*
+/*
         Q.debug = true;
         Q.debugFill = true;
         //*/
